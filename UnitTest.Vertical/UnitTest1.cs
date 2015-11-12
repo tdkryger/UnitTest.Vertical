364eeb09b415d810a4e3d21d2ab8b8e78f7b754b
Thomas D. Kryger <tdkryger@akvariet.dk>
2015-11-12 19:27:20 +0100

    Just pushing some code....

--- UnitTest.Vertical/UnitTest1.cs
+++ UnitTest.Vertical/UnitTest1.cs
@@ -1,14 +1,67 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace UnitTest.Vertical
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Contract.contract.CustomerContract getCustomerContract()
         {
+            return new CustomerInterfaceStub();
         }
+
+        [TestMethod]
+        public void CustomerContractGetAllTrips()
+        {
+            List<Contract.dto.Trip> tripList = new List<Contract.dto.Trip>();
+            Contract.contract.CustomerContract cis = getCustomerContract();
+            try
+            {
+                tripList = cis.GetAllTrips();
+            }
+            catch (Exception ex) // backend says: We dont know if the frontend wants us to thrown any specific
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            Assert.AreNotEqual(0, tripList.Count);
+        }
+
+        [TestMethod]
+        public void CustomerContractGetAllCustomerReservations()
+        {
+            List<Contract.dto.Reservation> resList = new List<Contract.dto.Reservation>();
+            Contract.contract.CustomerContract cis = getCustomerContract();
+            Contract.dto.Customer cust = new Contract.dto.Customer()
+            {
+                CustomerId = 1
+            };
+            try
+            {
+                resList = cis.GetAllCustomerReservations(cust);
+            }
+            catch (Contract.eto.CustomerNotFoundException)
+            {
+                Assert.Fail("Invalid customer");
+            }
+            Assert.AreNotEqual(0, resList);
+        }
+
+        [TestMethod]
+        public void CustomerContractCreateCustomer()
+        {
+            Contract.contract.CustomerContract cis = getCustomerContract();
+            Contract.dto.Customer cust = new Contract.dto.Customer()
+            {
+                CustomerId = 1
+            };
+            //backend: No special exception. 
+            bool result = cis.CreateCustomer(cust);
+            Assert.AreEqual(true, result);
+        }
+
+
     }
 }
